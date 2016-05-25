7556ad0d827bc9b0ad03f08bab36d8ef82ed7893
cotapon <cotapon@gmail.com>
2016-05-25 14:43:43 +0900

    font js testing in karma

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-        'src/ts/test/**/*.ts'
+        'app/public/javascripts/test/**/*.ts'
     ],
 
 
